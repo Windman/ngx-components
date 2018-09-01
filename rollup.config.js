d9feeeb30f8f6879a6edacd0db896e48c6047502
Max <maxkvt@gmail.com>
2018-09-01 18:58:00 +0300

    Change configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,5 @@
-import nodeResolve 	from 'rollup-plugin-node-resolve';
-import builtins 	from 'rollup-plugin-node-builtins';
-import commonjs    	from 'rollup-plugin-commonjs';
-import globals  	from 'rollup-plugin-node-globals';
-import json         from 'rollup-plugin-json';
-
 export default {
-    input: './dist/library/mk-angular-controls.module.ngfactory.js',
+    input: './dist/library/index.js',
     output: {
         file: 'dist/bundles/mk-angular-controls.umd.js',
         format: 'umd',
